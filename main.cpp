a21b910f066625a41d205e409ecf617889fb7825
Nikshay10 <nikshayreddygattu123@gmail.com>
2025-11-09 17:24:50 +0530

    got SLD2 to work

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        cerr << "SDL_Init Error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello SDL2!",               
+        SDL_WINDOWPOS_CENTERED,      
+        SDL_WINDOWPOS_CENTERED,      
+        800, 600,                    
+        SDL_WINDOW_SHOWN            
+    );
+
+    if (!window) {
+        cerr << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event event;
+    bool running = true;
+
+    while (running) {
+
+        
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        SDL_Delay(16); // ~60 FPS delay
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
