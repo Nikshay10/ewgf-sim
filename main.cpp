a67292e47c533ad6aa2c0d0a792c874e71445b2b
Nikshay10 <nikshayreddygattu123@gmail.com>
2025-12-05 00:22:39 +0530

    rewrote the game loop's 60fps logic

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
 #include <iostream>
 #include <bits/stdc++.h>
 
@@ -22,6 +23,7 @@ struct EWGFState{
     int frame = 0;
     int dfStart = -1;
     bool ewgf = false;
+    bool wgf = false;
 };
 
 EWGFState ewgfState;
@@ -33,6 +35,7 @@ bool isDF(int d) { return d == 3; }
 
 void updateEWGF(int direction, bool two){
     ewgfState.ewgf = false;
+    ewgfState.wgf = false;
     ewgfState.frame++;
 
     switch(ewgfState.state){
@@ -43,17 +46,20 @@ void updateEWGF(int direction, bool two){
             }
             break;
         case 1: // Forward window 
+            if(ewgfState.frame > 6){
+                ewgfState.state = 0;
+                break;
+            }
             if(isN(direction)){ 
                 ewgfState.state = 2;
                 ewgfState.frame = 0;
-                
             }
             else if(!isF(direction)){ // Checking for any direction other than neutral or forward
                 ewgfState.state = 0;
             }
             break;
         case 2: // Neutral window
-            if (ewgfState.frame > 2) {
+            if (ewgfState.frame > 6) {
                 ewgfState.state = 0; // too slow
                 break;
             }
@@ -74,8 +80,14 @@ void updateEWGF(int direction, bool two){
             }
             break;
         case 4:
+            if(ewgfState.frame > 4){
+                ewgfState.state = 0;
+                break;
+            }
             if(two && ewgfState.dfStart <=2)
                 ewgfState.ewgf = true;
+            else
+                ewgfState.wgf = true;
             ewgfState.state = 0;
             break;
     }
@@ -137,6 +149,15 @@ int getFace(const Uint8* keys){
 }
 
 int main() {
+    SDL_Init(SDL_INIT_AUDIO);
+
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+    std::cout << "Mix_OpenAudio error: " << Mix_GetError() << "\n";
+    }
+
+    Mix_Chunk* da = Mix_LoadWAV("sounds/jin_ewgf.mp3");
+    Mix_Chunk* dorya = Mix_LoadWAV("sounds/dorya.mp3");
+
     SDL_Window* window = SDL_CreateWindow(
         "ewgf sim",               
         SDL_WINDOWPOS_CENTERED,      
@@ -145,16 +166,28 @@ int main() {
         SDL_WINDOW_SHOWN            
     );
 
+    
+
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
+    
     DirectionalTextures textures = loadTextures(renderer);
-
+    
     SDL_Event event;
     bool running = true;
-
+    
     const Uint8* keyboardState = SDL_GetKeyboardState(NULL);
 
+    const float targetFrameTime = 1000.0f/60.0f;
+    Uint64 now = SDL_GetPerformanceCounter();
+    Uint64 last  = 0;
+    double deltaTime = 0;
+
     while (running) {
+        last = now;
+        now = SDL_GetPerformanceCounter();
+
+        deltaTime = (double)((now - last)*1000 / SDL_GetPerformanceFrequency());
+
         SDL_PumpEvents();
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) 
@@ -175,7 +208,7 @@ int main() {
             case 1: SDL_RenderCopy(renderer, textures.db, NULL, &r); break;
             case 2: SDL_RenderCopy(renderer, textures.d, NULL, &r); break;
             case 3: SDL_RenderCopy(renderer, textures.df, NULL, &r); break;
-            case 4: SDL_RenderCopy(renderer, textures.b, NULL, &r); break;
+            case 4: SDL_RenderCopy(renderer, textures.b, NULL, &r);break;
             case 6: SDL_RenderCopy(renderer, textures.f, NULL, &r); break;
             case 7: SDL_RenderCopy(renderer, textures.ub, NULL, &r); break;
             case 8: SDL_RenderCopy(renderer, textures.u, NULL, &r); break;
@@ -183,14 +216,25 @@ int main() {
         }
 
         updateEWGF(direction, getFace(keyboardState));
-        if(ewgfState.ewgf) cout << "EWGf";
-
+        if(ewgfState.ewgf){
+            cout << "EWGF" << endl;
+            Mix_PlayChannel(-1, da, 0);
+        }
+        else if(ewgfState.wgf){
+            cout << "WGF" << endl;
+            Mix_PlayChannel(-1, dorya, 0);
+        }
+        
         SDL_RenderPresent(renderer);
-        SDL_Delay(16); // To run the loop at 60 frames per second
+        double frameTime = (double)((SDL_GetPerformanceCounter() - now) * 1000 / SDL_GetPerformanceFrequency());
+        if(frameTime < targetFrameTime) SDL_Delay(targetFrameTime - frameTime);
+
     }
 
+    Mix_FreeChunk(da);    
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    Mix_CloseAudio();
     IMG_Quit();
     SDL_Quit();
     return 0;
